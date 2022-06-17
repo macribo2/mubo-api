8dcc27e0ed6aa3afbf77e9f9eb9e45308fb17fa2
ribo <roibeard.mac.unfraidh@gmail.com>
2022-06-17 02:13:31 +0200

    Connect meinFlix.db with index.js server via mongoose schema.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const Movies = Models.Movie;
+
+mongoose.connect('mongodb://localhost:27017/meinFlix', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
-
 const bodyParser = require('body-parser'),
   methodOverride = require('method-override');
 
@@ -30,6 +35,22 @@ app.get('/meinMovies', (req,res) =>{
 	res.json(meinMovies);
 });
 
+app.get('/movies', (req,res) =>{
+Movies.find().then((movies) => {
+  res.status(201).json(movies);
+}).catch((err) => {
+  console.error(err);
+//   res.status(500).send('Error: ' + err);
+});});
+// Get a Movie by Moviename
+app.get('/movies/:Title', (req, res) => {
+	Movies.findOne({ Title: req.params.Title })
+	  .then((user) => {	res.json(user);  })
+	  .catch((err) => {
+		console.error(err);
+		res.status(500).send('Error: ' + err);
+	  });
+  });
 //get 
 app.use((err, req, res, next) => {
 	console.error(err.stack);
