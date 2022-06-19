4d29db83942a7cd30a8ca7efa840c9c11df1e5ba
ribo <roibeard.mac.unfraidh@gmail.com>
2022-06-19 11:34:06 +0200

    Replace initial cors with another configuration.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
+const cors = require('cors');
+
+app.use(cors({ credentials: true, origin: true }));
 
 // mongoose.connect('mongodb://localhost:27017/meinFlix', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,7 +13,6 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser'),
   methodOverride = require('method-override');
-  app.use(cors());
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -51,19 +53,8 @@ app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send('Something broke!');
   });
-
-
-  // listen for requests
-  const port = process.env.PORT || 8080;
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/api', createProxyMiddleware({ 
-    target: port, 
-    changeOrigin: true, 
-    //secure: false,
-    onProxyRes: function (proxyRes, req, res) {
-       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
-}));
+// listen for requests
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
