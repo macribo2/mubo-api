1ca784f0bc8d38aa7a53e7e5dfc575eae744f040
ribo <roibeard.mac.unfraidh@gmail.com>
2022-06-19 10:46:48 +0200

    Troubleshoot with new mongoose connection string.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 
-mongoose.connect('mongodb://localhost:27017/meinFlix', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/meinFlix', { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 //myFlexDB.movies
 // mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
