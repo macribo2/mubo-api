8b0eaba75bb2048cbe8a9fa25132858ab47d0fa9
ribo <roibeard.mac.unfraidh@gmail.com>
2022-06-19 10:10:20 +0200

    Bug fix - process.env.CONNECTION_URI is not a string.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 
 // mongoose.connect('mongodb://localhost:27017/meinFlix', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require("express");
 const app = express();
